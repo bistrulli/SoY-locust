57c3ae6873ea857f3b68d20da711b38aa6537ff0
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-01-22 23:10:00 +0100

    refined locust file

--- locust_file/SoyMonoShorterIfLogin.py
+++ locust_file/SoyMonoShorterIfLogin.py
@@ -6,7 +6,7 @@ from pathlib import Path
 resourceDir=Path(__file__).parent.parent/Path("resources")
 
 class SoyMonoUser(HttpUser):
-    wait_time = between(1, 1.0001)
+    wait_time = between(1, 1)
     user_index = 0  # Static variable to keep track of user index
 
     def on_start(self):
@@ -52,7 +52,7 @@ class SoyMonoUser(HttpUser):
                 )
 
                 # OPTIONS before exercise production
-                self.client.request("OPTIONS", "/api/exercise-production")
+                #self.client.request("OPTIONS", "/api/exercise-production")
 
                 # Exercise production
                 with open(f'{resourceDir.absolute()}/soymono2/0046_request.json') as json_file:
