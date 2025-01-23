6796b2669d8c269798e29bf20bd4ba31d2a75c5f
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-01-23 18:10:12 +0100

    reverted soymono loust file before optios

--- locust_file/SoyMonoShorterIfLogin.py
+++ locust_file/SoyMonoShorterIfLogin.py
@@ -2,6 +2,7 @@ from locust import HttpUser, task, between, LoadTestShape
 import json
 import csv
 from pathlib import Path
+import requests
 
 resourceDir=Path(__file__).parent.parent/Path("resources")
 
@@ -25,44 +26,52 @@ class SoyMonoUser(HttpUser):
     def login_and_actions(self):
         email = self.user_data['email']
         password = self.user_data['password']
+        try:
+            # Login
+            login_response = self.client.post(
+                "/api/user/login",
+                headers={
+                    "Content-Type": "application/json",
+                },
+                json={"email": email, "password": password},
+            )
 
-        # Login
-        login_response = self.client.post(
-            "/api/user/login",
-            headers={
-                "Content-Type": "application/json",
-            },
-            json={"email": email, "password": password},
-        )
+            if login_response.status_code == 200:
+                access_token = login_response.cookies.get("access_token")
+                refresh_token = login_response.cookies.get("refresh_token")
 
-        if login_response.status_code == 200:
-            access_token = login_response.cookies.get("access_token")
-            refresh_token = login_response.cookies.get("refresh_token")
-
-            if access_token:
-                # Auth verify
-                self.client.get(
-                    "/api/auth/verify",
-                    headers={"Authorization": f"Bearer {access_token}"},
-                )
-
-                # Exercise production
-                with open(f'{resourceDir}/soymono2/0046_request.json') as json_file:
-                    exercise_data = json.load(json_file)
-                    self.client.post(
-                        "/api/exercise-production",
-                        headers={
-                            "Authorization": f"Bearer {access_token}",
-                            "Content-Type": "application/json",
-                        },
-                        json=exercise_data
+                if access_token:
+                    # Auth verify
+                    self.client.get(
+                        "/api/auth/verify",
+                        headers={"Authorization": f"Bearer {access_token}"},
                     )
 
-                # Logout
-                self.client.delete(
-                    "/api/user/logout",
-                    headers={"Authorization": f"Bearer {access_token}"},
-                )
+                    # Exercise production
+                    with open(f'{resourceDir}/soymono2/0046_request.json') as json_file:
+                        exercise_data = json.load(json_file)
+                        self.client.post(
+                            "/api/exercise-production",
+                            headers={
+                                "Authorization": f"Bearer {access_token}",
+                                "Content-Type": "application/json",
+                            },
+                            json=exercise_data
+                        )
+
+                    # Logout
+                    self.client.delete(
+                        "/api/user/logout",
+                        headers={"Authorization": f"Bearer {access_token}"},
+                    )
+        except requests.exceptions.RequestException as e:
+            print("An exception occurred:")
+            print(f"Type: {type(e)}")
+            print(f"Details: {e}")
+            if hasattr(e, 'request'):
+                print(f"Request: {e.request}")
+            if hasattr(e, 'response') and e.response is not None:
+                print(f"Response: {e.response.status_code} - {e.response.text}")
 
 # class CustomLoadShape(LoadTestShape):
 #     max_users = 200
