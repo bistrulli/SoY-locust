ab701137d24b43558ec35070b648c6b2ee45b0ed
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-02-04 09:21:19 +0100

    evaluate app with more replica

--- analysis.py
+++ analysis.py
@@ -4,28 +4,22 @@ from pathlib import Path
 import glob
 import re
 import matplotlib.pyplot as plt
-import argparse
 
-profileDir=None
+profileDir=Path("./profiled_data_1/")
 
 def extract_throughput_from_csv():
 	# Directory dei file CSV
 	csv_directory = str(profileDir)
 
-	print(csv_directory + "/results_*.csv_stats.csv")
-
 	# Trova tutti i file CSV che corrispondono al pattern
 	csv_files = glob.glob(csv_directory + "results_*.csv_stats.csv")
-
-	print(csv_files)
 
 	# Dizionario per memorizzare i throughput
 	throughput_data = []
 
 	# Carica ciascun file CSV e estrai il throughput
 	for file in csv_files:
-		print(file)
-		locustres = pd.read_csv(csv_directory+file)
+		locustres = pd.read_csv(file)
 		user_count=int(re.findall(r"[0-9]+",file)[0])
 		throughput = locustres["Requests/s"].values[0]
 		throughput_data+=[[user_count,throughput]]
@@ -51,22 +45,11 @@ def calculate_steady_state_throughput(users, service_time, k):
 	throughput=min(users/(1/(think_time+service_time)),k/(service_time))
 	return throughput
 
-def getCli():
-	global profileDir
-
-	parser = argparse.ArgumentParser(description="Analisi dei risultati di Locust")
-	parser.add_argument('profileDir', type=Path, help='Directory dei file di profilo')
-	args = parser.parse_args()
-
-	profileDir = Path(args.profileDir)
-
 
 if __name__ == '__main__':
-	getCli()
-
+	#calibrateQN()
 	troughput_data=extract_throughput_from_csv()
 	troughput_data=troughput_data.sort_values(by="Users",ascending=True)
-	print(troughput_data)
 	#pt=[calculate_steady_state_throughput(users=u, service_time=1.0/26.638775, k=1) for idx,u in enumerate(troughput_data["Users"].values)]
 	for idx,u in enumerate(troughput_data["Users"].values):
 		pt=calculate_steady_state_throughput(users=u, service_time=1.0/26.638775, k=1)
