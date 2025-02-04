38a2fdbdcefe61712da67a8954e7970c8d44a854
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-02-04 06:25:09 +0100

    refined analysis script

--- analysis.py
+++ analysis.py
@@ -7,17 +7,14 @@ profileDir=Path(".")
 def calibrateQN():
 	locustres=pd.read_csv(profileDir/Path("results.csv_stats.csv"))
 	cpudata=pd.read_csv(profileDir/Path("cpu_utilization.csv"))
-
-	print(locustres)
-	print(cpudata)
 	
-	troughput=locustres[locustres["Name"]=="/"]["Requests/s"].values
+	troughput=locustres["Requests/s"].values
 	util=cpudata["CPU Utilization (%)"].mean()
 
 
 	stime=(util)/troughput
 	stimelct=np.sum(locustres["Average Response Time"].values[0:-1])
-	#print(troughput[0]*(stimelct)/1000.0,(util)/(troughput*100))
+	print(troughput[0]*(stimelct)/1000.0,(util)/(troughput*100))
 	print(stime,troughput,util)
 
 
