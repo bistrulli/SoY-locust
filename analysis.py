309e8b063ed2994730aac62a5c45c3e056fbff33
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-02-04 07:50:45 +0100

    refined analysis script

--- analysis.py
+++ analysis.py
@@ -7,23 +7,23 @@ import re
 profileDir=Path(".")
 
 def extract_throughput_from_csv():
-    # Directory dei file CSV
-    csv_directory = "./profiled_data/"
+	# Directory dei file CSV
+	csv_directory = "./profiled_data/"
 
-    # Trova tutti i file CSV che corrispondono al pattern
-    csv_files = glob.glob(csv_directory + "results_*.csv_stats.csv")
+	# Trova tutti i file CSV che corrispondono al pattern
+	csv_files = glob.glob(csv_directory + "results_*.csv_stats.csv")
 
-    # Dizionario per memorizzare i throughput
-    throughput_data = []
+	# Dizionario per memorizzare i throughput
+	throughput_data = []
 
-    # Carica ciascun file CSV e estrai il throughput
-    for file in csv_files:
-        locustres = pd.read_csv(file)
-        user_count=int(re.findall(r"[0-9]+",file)[0])
-        throughput = locustres["Requests/s"].values[0]
-        throughput_data+=[[user_count,throughput]]
+	# Carica ciascun file CSV e estrai il throughput
+	for file in csv_files:
+		locustres = pd.read_csv(file)
+		user_count=int(re.findall(r"[0-9]+",file)[0])
+		throughput = locustres["Requests/s"].values[0]
+		throughput_data+=[[user_count,throughput]]
 
-    return pd.DataFrame(throughput_data,columns=["Users","Throughput"])
+	return pd.DataFrame(throughput_data,columns=["Users","Throughput"])
 
 def calibrateQN():
 	locustres=pd.read_csv(profileDir/Path("results.csv_stats.csv"))
@@ -39,18 +39,16 @@ def calibrateQN():
 	#print(stime,troughput,util)
 
 def calculate_steady_state_throughput(users, service_time, k):
-    think_time = 1  # Think time della think station
-    rho = service_time / k  # Utilizzazione per server
-
-    # Calcolo del throughput steady state
-    throughput = users / (think_time + (service_time / (1 - rho)))
-    return throughput
+	think_time = 1  # Think time della think station
+	# Calcolo del throughput steady state
+	throughput=min(users/(1/(think_time+service_time)),k/(1/(think_time+service_time)))
+	return throughput
 
 
 if __name__ == '__main__':
 	#calibrateQN()
-    troughput_data=extract_throughput_from_csv()
-    troughput_data=troughput_data.sort_values(by="Users",ascending=True)
-    for idx,u in enumerate(troughput_data["Users"].values):
-        pt=calculate_steady_state_throughput(users=u, service_time=1.0/26.638775, k=1)
-        print(f"User={u},Model={pt},Measured={troughput_data.iloc[idx,1]}")
+	troughput_data=extract_throughput_from_csv()
+	troughput_data=troughput_data.sort_values(by="Users",ascending=True)
+	for idx,u in enumerate(troughput_data["Users"].values):
+		pt=calculate_steady_state_throughput(users=u, service_time=1.0/26.638775, k=1)
+		print(f"User={u},Model={pt},Measured={troughput_data.iloc[idx,1]}")
