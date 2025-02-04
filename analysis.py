a93e50e52dacecbc75b9a8db0058cce291cdba9a
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-02-04 07:54:51 +0100

    refined analysis script

--- analysis.py
+++ analysis.py
@@ -51,4 +51,5 @@ if __name__ == '__main__':
 	troughput_data=troughput_data.sort_values(by="Users",ascending=True)
 	for idx,u in enumerate(troughput_data["Users"].values):
 		pt=calculate_steady_state_throughput(users=u, service_time=1.0/26.638775, k=1)
-		print(f"User={u},Model={pt},Measured={troughput_data.iloc[idx,1]}")
+		mt=troughput_data.iloc[idx,1]
+		print(f"User={u},Model={pt:.3f},Measured={mt:.3f},error={(pt-mt)*100/mt:.2f}%")
