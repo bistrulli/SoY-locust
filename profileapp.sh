50adaa9c25de7df56e1d6b83fe84d87fb8fd27a7
emilio-imt <emilio-imt@Host-004.homenet.telecomitalia.it>
2025-02-10 19:25:26 +0100

    refined monitoring

--- profileapp.sh
+++ profileapp.sh
@@ -2,7 +2,7 @@
 
 #get name of running container
 # Get container IDs excluding 'monotlothv4_postgres' and store them in an array
-container_ids=$(docker ps --format "{{.ID}} {{.Names}}" | grep -v "monotlothv4_postgres" | awk '{print $1}')
+container_ids=($(docker ps --format "{{.ID}} {{.Names}}" | grep -v "monotlothv4_postgres" | awk '{print $1}'))
 
 echo "Container IDs: ${container_ids[@]}"
 
