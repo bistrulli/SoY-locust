7f80892cef8ea876b020b692f65844a88a3d92cb
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-03-03 18:02:07 +0100

    updated scripts for reproducing the tables

--- run_locust_files.sh
+++ run_locust_files.sh
@@ -11,7 +11,7 @@ for file in locust_file/SoyMonoShorterIfLogin*.py; do
         sed -i.bak -E "/node:/,/deploy:/{s/(replicas:)[[:space:]]*[0-9]+/\1 $num/}" "$yml_file"
 
         base=$(basename "$file" .py)
-        csv_dir="runtime_data/${base}"
+        csv_dir="runtime_data/${base}/${base}"
         mkdir -p "$csv_dir"
         echo "Esecuzione del test per: $file con replica $num, CSV in: $csv_dir"
         python3 run_load_test.py --users 1 --spawn-rate 100 --run-time 3m --host http://localhost:5001 --csv "$csv_dir" --locust-file "$file"
