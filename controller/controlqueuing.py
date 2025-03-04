a12a4ba102ca68f84ea08f64ce785595e66fe59f
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2025-03-04 22:06:37 +0100

    minor

--- controller/controlqueuing.py
+++ controller/controlqueuing.py
@@ -25,14 +25,14 @@ class OPTCTRL():
             
             obj=0;
             for i in range(nApp):
-                self.model.subject_to(T[0, i] == casadi.fmin(C[i] / (1.0+e[i]),S[0, i] / e[i]))
-                #self.model.subject_to(T[0, i] == S[0, i] / e[i])
+                #self.model.subject_to(T[0, i] == casadi.fmin(C[i] / (1.0+e[i]),S[0, i] / e[i]))
+                self.model.subject_to(T[0, i] == S[0, i] / e[i])
                 #tgt*(S[0,i]/(e[i]))=C[i]
                 #self.model.subject_to(S[0, i] <= C[i])
                 #self.model.subject_to(T[0, i] <= C[i] / (e[i]))
                 #self.model.subject_to(T[0, i]<= S[0, i] / e[i])
                 #obj+=(C[i]-(1+tgt[i])*T[0, i])**2+0.000000*S[0, i]
-                obj+=(C[i]-(tgt*T[0,i]))**2+0.1*S[0, i]
+                obj+=(C[i]-(tgt*T[0,i]))**2+0.01*S[0, i]
         
             self.model.minimize(obj)    
             # self.model.solver('osqp',{'print_time':False,'error_on_fail':False})
