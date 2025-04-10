8077c229c7ad0c754310c4be8a678c35655f4125
benoit lange <benoit@MacBook-Pro-de-benoit.local>
2025-04-10 15:16:49 +0200

    feat: translate script in english, and update swarm with an init phase

--- run_locust_files.sh
+++ run_locust_files.sh
@@ -2,8 +2,8 @@
 
 # Verifica se è stato fornito il parametro per il file di loadshape
 if [ $# -lt 1 ]; then
-    echo "Utilizzo: $0 <path_loadshape_file>"
-    echo "Esempio: $0 locust_file/loadshapes/cyclical_shape.py"
+    echo "Use: $0 <path_loadshape_file>"
+    echo "Example: $0 locust_file/loadshapes/cyclical_shape.py"
     exit 1
 fi
 
@@ -11,9 +11,12 @@ fi
 LOADSHAPE_FILE="$1"
 
 # Stampa informazioni sull'esecuzione
-echo "Utilizzo LoadShape: $LOADSHAPE_FILE"
-echo "Avvio dei test batch per tutti i file locust..."
+echo "LoadShape usage: $LOADSHAPE_FILE"
+echo "Starting batch tests for all locust files..."
 
+current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+
+base_path="results/${current_date}"
 # Recupera tutti i file nella cartella locust_file il cui nome inizia per "SoyMonoShorterIfLogin"
 for file in locust_file/SoyMonoShorterIfLogin*.py; do
     if [ -f "$file" ]; then
@@ -21,12 +24,12 @@ for file in locust_file/SoyMonoShorterIfLogin*.py; do
         num=$(echo "$file" | grep -oP '(?<=_x).*?(?=\.py$)')
         [ -z "$num" ] && num=1  # Default se non trovato
 
-        echo "replica to set $num"  # questo stampa correttamente il valore estratto se $num è impostato
+        echo "replica to set $num"  # this correctly prints the extracted value if $num is set
 
         base=$(basename "$file" .py)
-        # Se la cartella "results/${base}" esiste già, salta l'esperimento.
-        if [ -d "results/${base}" ]; then
-            echo "La cartella results/${base} esiste già, salto l'esperimento per $file."
+        # Se la cartella "${base_path}/${base}" esiste già, salta l'esperimento.
+        if [ -d "${base_path}/${base}" ]; then
+            echo "The folder ${base_path}/${base} already exists, skipping the experiment for $file."
             continue
         fi
 
@@ -34,12 +37,13 @@ for file in locust_file/SoyMonoShorterIfLogin*.py; do
         yml_file="sou/monotloth-v4.yml"
         sed -i.bak -E '/^\s*node:/,/^\s*deploy:/ { n; s/^( *replicas:)[ ]*[0-9]+/\1 '"$num"'/ }' "$yml_file"
 
-        csv_dir="results/${base}/${base}"
-        mkdir -p "results/${base}"
-        echo "Esecuzione del test per: $file con replica $num, CSV in: $csv_dir"
-        
+        csv_dir="${base_path}/${base}/${base}"
+        mkdir -p "${base_path}/${base}"
+        echo "Execution of the test for: $file with replica $num, CSV in: $csv_dir"
+
         # Aggiorno il comando per passare anche il parametro loadshape-file
-        python3 run_load_test.py --users 1 --spawn-rate 100 --run-time 3m --host http://localhost:5001 --csv "$csv_dir" --locust-file "$file" --loadshape-file "$LOADSHAPE_FILE" >> "results/${base}/locust.log" 2>&1
+        #python3 run_load_test.py --users 1 --spawn-rate 100 --run-time 3m --host http://localhost:5001 --csv "$csv_dir" --locust-file "$file" --loadshape-file "$LOADSHAPE_FILE" >> "${base_path}/${base}/locust.log" 2>&1
+        echo "python3 run_load_test.py --users 1 --spawn-rate 100 --run-time 3m --host http://localhost:5001 --csv "${csv_dir}" --locust-file "$file" --loadshape-file "$LOADSHAPE_FILE" >> "${base_path}/${base}/locust.log" 2>&1"
         sleep 3m # aggiunto per attendere 3 minuti dopo l'esecuzione del test
     fi
 done
--- swipe_users.sh
+++ swipe_users.sh
@@ -7,6 +7,6 @@ ENDUSR=$4
 STEPUSR=$5
 
 for USERS in $(seq $STARTUSR $STEPUSR $ENDUSR); do
-    echo "Esecuzione di profileapp.sh per $USERS utenti..."
+    echo "Executing profileapp.sh for $USERS users..."
     ./profileapp.sh $LOCUST_TIME $USERS $LOCUST_FILE
 done
